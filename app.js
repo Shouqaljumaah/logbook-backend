a6450144c64f7df878fc47beb9f15e22f5ed93b8
Shouqaljumaah <shouqaljumaah@gmail.com>
2024-12-30 21:13:14 +0300

    setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+// const connectDB = require("./database");
+
+const PORT = 8000;
+
+app.use(express.json());
+
+// connectDB();
+
+app.listen(PORT, () => {
+  console.log(`The application is running on localhost:${PORT}`);
+});
